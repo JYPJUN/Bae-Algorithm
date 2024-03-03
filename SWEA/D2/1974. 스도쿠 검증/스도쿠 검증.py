9d8edc786344355164ddf0b4f63df49d739e63eb
JYPJUN <156387559+JYPJUN@users.noreply.github.com>
2024-03-03 17:17:33 +0900

    [D2] Title: 스도쿠 검증, Time: 154 ms, Memory: 58,264 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -1,46 +1,29 @@
-tc = int(input())
+T = int(input())
 
-for i in range(1, tc+1):
-    lst = [list(map(int, input().split())) for _ in range(9)]
-    # 사각형 내 이동
-    dir = [(r, m) for r in range(3) for m in range(3)]
+for tc in range(1, T+1):
+    arr = [list(map(int, input().split())) for _ in range(9)]
+    sero_arr = list(map(list, zip(*arr)))
+    result = 1
 
-    # 가로행 + 세로행 확인
-    row = 0
-    column = []
-    coll = 0
-    square = 0
-    for n in range(9):
-        row = len(set(lst[n]))
-        if row != 9:
-            break
-    for n in range(9):
-        column = []
-        for m in range(9):
-            column.append(lst[m][n])
-        coll = len(set(column))
-        if coll != 9:
-            break
-    # 사각형 확인 0 1 2 3 4 5 6 7 8
-    for p in range(0, 9, 3):
-        for q in range(0, 9, 3):
-            bin_list = []
-            for d in dir:
-                # 아래로
-                dp = p + d[0]
-                # 옆으로
-                dq = q + d[1]
-                bin_list.append(lst[dp][dq])
-            square = len(set(bin_list))
-            if square != 9:
+    for i in range(len(arr)):
+        for p in range(1, 10):
+            if p not in arr[i] or p not in sero_arr[i]:
+                result = 0
                 break
-        else:
-            continue
-        break
-        
-    if row + coll + square == 27:
-        result = 1
-    else:
-        result = 0
+        if result == 0:
+            break
 
-    print(f'#{i}', result)
+    if result != 0:
+        for n in range(0, 7, 3):
+            for m in range(0, 7, 3):
+                check = set()
+                for a in range(0, 3):
+                    for b in range(0, 3):
+                        check.add(arr[n+a][m+b])
+                if len(check) != 9:
+                    result = 0
+                    break
+            if result == 0:
+                break
+
+    print(f'#{tc}', result)
