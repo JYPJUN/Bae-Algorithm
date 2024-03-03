a1f8018f97c7416f56957a120ef7b7fc9adfbfb3
JYPJUN <156387559+JYPJUN@users.noreply.github.com>
2024-03-03 19:49:22 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 154 ms, Memory: 58,512 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -1,46 +1,16 @@
-tc = int(input())
- 
-for i in range(1, tc+1):
+T = int(input())
+for tc in range(1, T+1):
     N, K = map(int, input().split())
-    lst = [list(map(int, input().split())) for _ in range(N)]
-    dir_r = []
-    dir_d = []
-    for n in range(K):
-        dir_r.append((0, n))
-    for m in range(K):
-        dir_d.append((m, 0))
- 
+    arr = [''.join(input().split()) for _ in range(N)]
+    for i in range(N):
+        words = ''
+        for j in range(N):
+            words += arr[j][i]
+        arr.append(words)
     cnt = 0
-    # 가로행
-    for j in range(N):
-        for k in range(N-K+1):
-            result = 0
-            for d in dir_r:
-                nk = k + d[1]
-                result += lst[j][nk]
-            if result == K:
-                if nk == N-1 and lst[j][k-1] != 1:
-                    cnt += 1
-                elif k == 0 and lst[j][nk+1] != 1:
-                    cnt += 1
-                elif 1 <= k and nk < N-1:
-                    if lst[j][k-1] != 1 and lst[j][nk+1] != 1:
-                        cnt += 1
- 
-    # 세로행
-    for j in range(N-K+1):
-        for k in range(N):
-            result = 0
-            for d in dir_d:
-                nj = j + d[0]
-                result += lst[nj][k]
-            if result == K:
-                if nj == N - 1 and lst[j-1][k] != 1:
-                    cnt += 1
-                elif j == 0 and lst[nj+1][k] != 1:
-                    cnt += 1
-                elif 1 <= j and nj < N - 1:
-                    if lst[j-1][k] != 1 and lst[nj+1][k] != 1:
-                        cnt += 1
- 
-    print(f'#{i}', cnt)
+    for p in arr:
+        a = p.split('0')
+        for q in a:
+            if len(q) == K:
+                cnt += 1
+    print(f'#{tc}', cnt)
