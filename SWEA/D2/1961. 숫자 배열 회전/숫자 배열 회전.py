9a16b848cb6fa14267b2f830460fb0166925c9d1
JYPJUN <156387559+JYPJUN@users.noreply.github.com>
2024-03-03 17:46:04 +0900

    [D2] Title: 숫자 배열 회전, Time: 154 ms, Memory: 58,244 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -1,29 +1,12 @@
-tc = int(input())
+T = int(input())
 
-for i in range(1, tc+1):
+for tc in range(1, T+1):
     N = int(input())
-    lst = [list(map(int, input().split())) for _ in range(N)]
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    arr_90 = [[arr[i][j] for i in range(N-1, -1, -1)] for j in range(N)]
+    arr_180 = [[arr_90[i][j] for i in range(N-1, -1, -1)] for j in range(N)]
+    arr_270 = [[arr_180[i][j] for i in range(N - 1, -1, -1)] for j in range(N)]
 
-    lst_90 = [[0]*N for _ in range(N)]
-    lst_180 = [[0] * N for _ in range(N)]
-    lst_270 = [[0] * N for _ in range(N)]
-
-    for m in range(N):
-        for n in range(N):
-            lst_90[m][n] = lst[N-n-1][m]
-            lst_180[m][n] = lst[N-m-1][N-n-1]
-            lst_270[m][n] = lst[n][N-m-1]
-
-    result_lst = []
-    for idx in range(N):
-        result_lst.append(lst_90[idx])
-        result_lst.append(lst_180[idx])
-        result_lst.append(lst_270[idx])
-
-    print("#{}".format(i))
-    for idx, num in enumerate(result_lst):
-        if idx % 3 == 0 and idx != 0:
-            print()
-        numbers = ''.join(map(str, num))
-        print(numbers, end = ' ')
-    print()
+    print(f'#{tc}')
+    for a in range(N):
+        print(''.join(map(str, arr_90[a])) + ' ' + ''.join(map(str, arr_180[a])) + ' ' + ''.join(map(str, arr_270[a])))
