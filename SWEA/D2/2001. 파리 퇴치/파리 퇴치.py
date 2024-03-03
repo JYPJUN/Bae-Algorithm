f4dc600b3925388058e90508e1f56ff8f33587c8
JYPJUN <156387559+JYPJUN@users.noreply.github.com>
2024-03-03 20:01:13 +0900

    [D2] Title: 파리 퇴치, Time: 177 ms, Memory: 61,460 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -1,21 +1,18 @@
-tc = int(input())
- 
-for i in range(1, tc+1):
+T = int(input())
+
+for tc in range(1, T+1):
     N, M = map(int, input().split())
-    lst = [list(map(int, input().split())) for _ in range(N)]
-    dir = []
-    for j in range(M):
-        for n in range(M):
-            dir.append((j, n))
- 
-    bug_lst = []
-    for k in range(N-M+1):
-        for m in range(N-M+1):
-            result = 0
-            for d in dir:
-                ni = k + d[0]
-                nj = m + d[1]
-                if 0 <= ni < N and 0 <= nj < N:
-                    result += lst[ni][nj]
-            bug_lst.append(result)
-    print(f'#{i}', max(bug_lst))
+    dirs = [(i, j) for i in range(M) for j in range(M)]
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    max_cnt =0
+    for p in range(N):
+        for q in range(N):
+            cnt = 0
+            for d in dirs:
+                ni, nj = p + d[0], q + d[1]
+                if 0<=ni<N and 0<=nj<N:
+                    cnt += arr[ni][nj]
+            if max_cnt < cnt:
+                max_cnt = cnt
+
+    print(f'#{tc}', max_cnt)
