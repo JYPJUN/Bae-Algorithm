1427a376e02da899aed0726b8b0b3db8817fe0ca
JYPJUN <156387559+JYPJUN@users.noreply.github.com>
2024-03-03 16:56:50 +0900

    [D2] Title: 두 개의 숫자열, Time: 164 ms, Memory: 58,268 KB -BaekjoonHub

--- SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
@@ -6,20 +6,18 @@ for tc in range(1, T+1):
     b_arr = list(map(int, input().split()))
     max_value = 0
     value = 0
+
+    if a < b :
+        a, b = b, a
+        a_arr, b_arr = b_arr, a_arr
+
     if a == b:
         for i in range(a):
             value += a_arr[i] * b_arr[i]
-    elif a > b:
+    else:
         for p in range(a-b+1):
             for q in range(b):
                 value += a_arr[p+q] * b_arr[q]
-            if max_value < value:
-                max_value = value
-            value = 0
-    else:
-        for p in range(b-a+1):
-            for q in range(a):
-                value += a_arr[q] * b_arr[p+q]
             if max_value < value:
                 max_value = value
             value = 0
