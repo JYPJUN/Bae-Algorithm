dca52fb0bb56966b6abe5b1e029557ed82d58421
JYPJUN <156387559+JYPJUN@users.noreply.github.com>
2024-03-21 00:55:59 +0900

    [Gold V] Title: AC, Time: 148 ms, Memory: 40220 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -3,13 +3,16 @@ from collections import deque
 
 T = int(sys.stdin.readline())
 for tc in range(T):
-    p = sys.stdin.readline().rstrip()  # 명령어
+    p = sys.stdin.readline().rstrip().replace('RR', '')  # 명령어
     N = int(sys.stdin.readline())  # 숫자 배열의 수
-    word = eval(sys.stdin.readline().rstrip()) # json으로 받는게 더 좋아보임.. ex) import json 후 json.loads(input().rstrip())
+    arr = sys.stdin.readline().strip("[]\n")
+    if arr:
+        arr = deque(arr.split(","))
+    else:
+        arr = deque([])
     check = 1  # 오류인지 아닌지 알려줄 check 항목 / 그리고 R의 좌우 방향 바꾸는거 check
     # check 의 1은 정방향 / 0은 역방향 / -1은 에러!
 
-    arr = deque(word)  # 실행할 수열
     for i in p:
         if i == 'R':  # R은 뒤집기
             check = (check+1) % 2
@@ -25,23 +28,11 @@ for tc in range(T):
                     arr.pop()
 
     if check != -1: # 오류가 아니면 아래 출력하기
-        if check: # 정방향 출력이면
-            if len(arr) == 0: # 비어있으면 빈 리스트 출력
-                print('[]')
-            else: # 아니라면 원하는 순서대로 출력
-                print('[', end='')
-                for i in range(len(arr)):
-                    if i == len(arr) - 1:  # 마지막 숫자라면
-                        print(str(arr[i]) + ']')
-                    else:  # 마지막 숫자가 아니라면
-                        print(str(arr[i]) + ',', end='')
-        else: # 역방향 출력이면
-            if len(arr) == 0: # 비어있으면 빈 리스트 출력
-                print('[]')
-            else:
-                print('[', end='')
-                for i in range(len(arr)-1, -1, -1):
-                    if i == 0:  # 마지막 숫자라면
-                        print(str(arr[i]) + ']')
-                    else:  # 마지막 숫자가 아니라면
-                        print(str(arr[i]) + ',', end='')
+        if len(arr) == 0:  # 비어있으면 빈 리스트 출력
+            print('[]')
+        else:
+            if check: # 정방향 출력이면
+                print(f'[{",".join(arr)}]')
+            else: # 역방향 출력이면
+                arr.reverse()
+                print(f'[{",".join(arr)}]')
