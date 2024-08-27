796325f26c5f9134ede8e32eb919197e6e09de31
JYPJUN <156387559+JYPJUN@users.noreply.github.com>
2024-08-27 10:21:47 +0900

    [Gold V] Title: LCS, Time: 476 ms, Memory: 55708 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -2,21 +2,19 @@ import sys
 
 I = sys.stdin.readline
 
-word1 = I()
+word1 = I().strip()
 word1_len = len(word1)
-word2 = I()
+word2 = I().strip()
 word2_len = len(word2)
-lcs = [[0] * (word1_len+1) for _ in range(word2_len+1)]
+lcs = [[0] * (word2_len + 1) for _ in range(word1_len + 1)]
 
-for i in range(1, word1_len):
-    for j in range(1, word2_len):
-        if word1[i-1] == word2[j-1]:
-            lcs[j][i] = lcs[j-1][i-1] + 1
+for i in range(1, word1_len + 1):
+    for j in range(1, word2_len + 1):
+        if word1[i - 1] == word2[j - 1]:
+            lcs[i][j] = lcs[i - 1][j - 1] + 1
         else:
-            lcs[j][i] = max(lcs[j-1][i], lcs[j][i-1])
+            lcs[i][j] = max(lcs[i - 1][j], lcs[i][j - 1])
 
-result = 0
-for n in range(word2_len+1):
-    result = max(max(lcs[n]), result)
+result = lcs[word1_len][word2_len]
 
 print(result)
