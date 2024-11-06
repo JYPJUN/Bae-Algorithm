50b573075abb36d94586bc8af60477997524d3ac
JYPJUN <156387559+JYPJUN@users.noreply.github.com>
2024-11-06 09:11:53 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a;
+	cin >> b;
+
+	if (a > 0) {
+		if (b > 0) {
+			cout << 1 << endl;
+		}
+		else {
+			cout << 4 << endl;
+		}
+
+	}
+	else {
+		if (b > 0) {
+			cout << 2 << endl;
+		}
+		else
+			cout << 3 << endl;
+	}
+
+	return 0;
+}
