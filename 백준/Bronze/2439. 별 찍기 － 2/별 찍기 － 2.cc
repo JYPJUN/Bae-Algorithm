11bad9b5bad487bf14f7d9b8b713363a4ebb92ba
JYPJUN <156387559+JYPJUN@users.noreply.github.com>
2024-11-13 23:28:06 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	
+	int a;
+	cin >> a;
+
+	for (int i = 1; i < a + 1; i++) {
+		cout << string(a-i, ' ') << string(i, '*') << '\n';
+	}
+	
+	return 0;
+}
