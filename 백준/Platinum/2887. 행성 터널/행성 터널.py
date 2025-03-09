ab6441db90827aa2b5fa0ed468fc6da3128832e2
JYPJUN <156387559+JYPJUN@users.noreply.github.com>
2025-03-09 12:17:00 +0900

    [Platinum V] Title: 행성 터널, Time: 1204 ms, Memory: 89440 KB -BaekjoonHub

--- 백준/Platinum/2887. 행성 터널/행성 터널.py
+++ 백준/Platinum/2887. 행성 터널/행성 터널.py
@@ -7,18 +7,16 @@ def find(parent, x):
         parent[x] = find(parent, parent[x])
     return parent[x]
 
-def union(parent, rank, a, b):
+def union(parent, a, b):
     rootA = find(parent, a)
     rootB = find(parent, b)
-    
+
     if rootA != rootB:
-        if rank[rootA] > rank[rootB]:
+        if rootA < rootB:
             parent[rootB] = rootA
-        elif rank[rootB] > rank[rootA]:
-            parent[rootA] = rootB
         else:
-            parent[rootB] = rootA
-            rank[rootA] += 1
+            parent[rootA] = rootB
+
 
 planets = []
 for i in range(N):
@@ -34,12 +32,11 @@ for dim in range(3):
 edges.sort()
 
 parent = [i for i in range(N)]
-rank = [0] * N
 total_cost = 0
 select_edges = 0
 for cost, a , b in edges:
     if find(parent, a) != find(parent, b):
-        union(parent, rank, a, b)
+        union(parent, a, b)
         total_cost += cost
         select_edges += 1
         if select_edges == N-1:
