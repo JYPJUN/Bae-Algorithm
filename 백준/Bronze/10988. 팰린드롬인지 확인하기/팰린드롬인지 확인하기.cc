a6f2b24cb8f225f7525038dc8bec049eaeb384a4
JYPJUN <156387559+JYPJUN@users.noreply.github.com>
2025-06-28 17:04:10 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  string word;
+  cin >> word;
+  int i=0, j = word.size()-1;
+  bool flag = true;
+  while (i != j && i < j) {
+    if (word[i] != word[j]) {
+      flag = false;
+      break;
+    }
+    i++;
+    j--;
+  }
+
+  cout << flag << '\n';
+
+  return 0;
+}
