694646b736ae44dc79b0a2acfe810c73b03518e3
JYPJUN <156387559+JYPJUN@users.noreply.github.com>
2025-06-28 15:51:48 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int chess[6] = {1, 1, 2, 2, 2, 8};
+
+  for (int i = 0; i < 6; i++) {
+    int num;
+    cin >> num;
+    cout << chess[i]-num << ' ';
+  }
+
+  return 0;
+}
