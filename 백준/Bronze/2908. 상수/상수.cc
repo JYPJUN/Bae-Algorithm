4e025af972725a4e462d6289361ab2687892ecb8
JYPJUN <156387559+JYPJUN@users.noreply.github.com>
2025-06-28 14:24:19 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+string reverse_str(string word) {
+  int i = 0, j = word.size()-1;
+
+  while (i < j) {
+    char temp;
+    temp = word[i];
+    word[i] = word[j];
+    word[j] = temp;
+    i++;
+    j--;
+  }
+
+  return word;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  string a_num, b_num;
+  cin >> a_num >> b_num;
+
+  a_num = reverse_str(a_num);
+  b_num = reverse_str(b_num);
+
+  if (a_num > b_num) cout << a_num << '\n'; else cout << b_num << '\n';
+
+  return 0;
+}
+
