d9b9970fb3a3e33ea99fa60fe05f07cd6115a369
JYPJUN <156387559+JYPJUN@users.noreply.github.com>
2025-08-25 14:23:15 +0900

    [Silver III] Title: 바이러스, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,35 +1,39 @@
 import sys
 input = sys.stdin.readline
 
-def find(x, parent):
-    if parent[x] != x:
-        parent[x] = find(parent[x], parent)  # 경로 압축
-    return parent[x]
-
-def union(x, y, parent):
-    rx = find(x, parent)
-    ry = find(y, parent)
-    if rx == ry:
+def find(x, info):
+    if info[x] != x:
+        info[x] = find(info[x], info)
+    return info[x]
+        
+def union(x, y, info):
+    root_x = find(x, info)
+    root_y = find(y, info)
+    
+    if root_x == root_y:
         return
-    # 간단히 번호 작은 쪽에 붙이기 (2606에선 충분)
-    if rx < ry:
-        parent[ry] = rx
+    
+    if root_x < root_y:
+        info[root_y] = root_x
     else:
-        parent[rx] = ry
+        info[root_x] = root_y
 
 def solve():
     N = int(input())
     S = int(input())
-
-    parent = list(range(N + 1))
-
+    
+    result = 0
+    _info = list(range(N+1))
+    
     for _ in range(S):
         a, b = map(int, input().split())
-        union(a, b, parent)
-
-    root1 = find(1, parent)
-    # 1번 컴퓨터를 제외하고, 루트가 root1인 노드 개수
-    result = sum(1 for i in range(2, N + 1) if find(i, parent) == root1)
+        union(a, b, _info)
+    
+    for i in range(1, N+1):
+        find(i, _info)
+    
+    result = _info.count(1) - 1
+    
     return result
 
 if __name__ == '__main__':
